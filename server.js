0c79dd5256401a6e44cf3ca3b7bcb49bccd6ecc7
Dito B. Prasetio <ditoprasetio28@gmail.com>
2026-09-15 13:34:45 +0700

    feat: add some function in server.js

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-function sum(a, b) {
+function plus(a, b) {
     return a + b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function multiple(a, b) {
+    return a * b;
 }
 
 module.export = sum;
